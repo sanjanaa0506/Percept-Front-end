f4de5f40eaa3e4db762164e8ccad417ea09a0fac
Sanjana <66768956+sanjanaa0506@users.noreply.github.com>
2022-04-12 13:46:22 +0530

    Var ,let, const  (Off-page JScript)

---
+++ JAVASCRIPT/first.js
@@ -0,0 +1,29 @@
+var s="SANJUNE";
+console.log(s);
+
+let a=20;
+console.log(a);
+
+
+const b="HELLO";
+console.log(b);
+
+a=a+100;
+console.log(a);
+
+var c=20;
+c=c-5;
+console.log(c);
+
+a=a*2;
+console.log(a);
+
+a=a/3;
+console.log(a);
+
+c=c+2;
+console.log(c);
+
+
+c=c--;
+console.log(c);

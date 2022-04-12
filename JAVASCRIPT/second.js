bbbf837ee34fc7985e9431daf144a230747e2a24
Sanjana <66768956+sanjanaa0506@users.noreply.github.com>
2022-04-12 13:47:27 +0530

    function, for loop while loop(Off-page Javascript)

---
+++ JAVASCRIPT/second.js
@@ -0,0 +1,32 @@
+let a=20;
+console.log(a);
+
+function bb()
+{
+let a=10;
+console.log(a);
+}
+bb()
+
+
+for(var i=2; i<=10; i++)
+{
+	if(i%2!=0)
+	{
+		console.log(i);
+    }
+    else
+    {
+    	console.log('NAHI');
+    }  
+}
+
+
+
+var i=1;
+while(i!=10)	
+{
+    console.log("i is ", i);
+    i++;	
+}
+console.log("Shut UP");

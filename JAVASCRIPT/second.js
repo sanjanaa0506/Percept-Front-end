24a1a4d04238c738f8f5f3d0f5095deaef7261b8
Sanjana <66768956+sanjanaa0506@users.noreply.github.com>
2022-04-13 16:08:20 +0530

    Function with Parameter

--- JAVASCRIPT/second.js
+++ JAVASCRIPT/second.js
@@ -1,15 +1,35 @@
 let a=20;
-console.log(a);
+console.log(a);                               //variable using let 
 
-function bb()
+
+
+
+
+
+function bb()                                  //function definition (without parameters)
 {
 let a=10;
 console.log(a);
 }
-bb()
+bb()                                           //function call
+ 
 
 
-for(var i=2; i<=10; i++)
+
+ 
+function cc(myname,yourname)                    //function with 2 parameters
+{
+    console.log("HELLO " + myname + " AND" + yourname);
+}
+cc("Hruu "," Sanjune");              //function call        
+cc("Hruu "," Priya");                //function call
+
+
+
+
+
+
+for(var i=2; i<=10; i++)             //for loop to print even numbers and "NAHI" to odd numbers
 {
 	if(i%2!=0)
 	{
@@ -22,11 +42,11 @@ for(var i=2; i<=10; i++)
 }
 
 
-
-var i=1;
+ 
+var i=0;                   //while loop to print i for values 0 to 10 with text i is ... upto 10
 while(i!=10)	
 {
     console.log("i is ", i);
     i++;	
 }
-console.log("Shut UP");
+console.log("BYEE");

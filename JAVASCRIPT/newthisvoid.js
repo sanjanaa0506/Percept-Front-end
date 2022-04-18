bc564c2eab4cf2c0f500843b0f2347f0733935ef
Sanjana <66768956+sanjanaa0506@users.noreply.github.com>
2022-04-18 14:05:34 +0530

    keywords

---
+++ JAVASCRIPT/newthisvoid.js
@@ -0,0 +1,18 @@
+function fruit(color,taste,seeds)
+{
+	this.color=color;
+	this.taste=taste;
+	this.seeds=seeds;
+}
+const fruit1 =new fruit("yellow","sweet",1);
+console.log(fruit1);
+
+
+void function iffff( ) 
+{
+	var msg=function()
+	{
+		console.log("hello");
+	};
+	msg();
+}();
